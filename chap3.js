2d574688c3fa590be89aacdb13b24b47936f393e
Amy English <amy.english.developer@gmail.com>
2021-07-22 12:04:44 +0100

    WIP - ch3
    in progress

---
+++ chap3.js
@@ -0,0 +1,21 @@
+//Declaring userAge
+var userAge = 23;
+console.log(userAge);
+
+//Updating userAge
+userAge = 33;
+console.log(userAge);
+
+var num1 = 5;
+console.log(num1 + 10);
+
+var string1 = '5';
+console.log(string1 + 10);
+
+var a = 5;
+var b = 10;
+
+console.log(a>b);
+console.log(b>a);
+
+console.log("demoArray")
